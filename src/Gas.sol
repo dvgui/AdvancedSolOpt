e08cde6cdd1b442b502f0b36a18d041f06660853
Gustavo Dutra <gobannondutra@gmail.com>
2024-09-09 13:29:53 -0300

    Simplifying addToWhiteList

--- src/Gas.sol
+++ src/Gas.sol
@@ -37,8 +37,8 @@ contract GasContract is Ownable {
         uint32 blockNumber;
         address updatedBy;
     }
-    uint256 wasLastOdd = 1;
-    mapping(address => uint256) public isOddWhitelistUser;
+    bool wasLastOdd = true;
+    mapping(address => bool) public isOddWhitelistUser;
 
     struct ImportantStruct {
         address sender;
@@ -217,26 +217,11 @@ contract GasContract is Ownable {
             _tier < 255,
             "Gas Contract - addToWhitelist function -  tier level should not be greater than 255"
         );
-        whitelist[_userAddrs] = _tier;
+
         if (_tier > 3) {
-            whitelist[_userAddrs] -= _tier;
             whitelist[_userAddrs] = 3;
-        } else if (_tier == 1) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 1;
-        } else if (_tier > 0 && _tier < 3) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 2;
-        }
-        uint256 wasLastAddedOdd = wasLastOdd;
-        if (wasLastAddedOdd == 1) {
-            wasLastOdd = 0;
-            isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
-        } else if (wasLastAddedOdd == 0) {
-            wasLastOdd = 1;
-            isOddWhitelistUser[_userAddrs] = wasLastAddedOdd;
         } else {
-            revert("Contract hacked, imposible, call help");
+            whitelist[_userAddrs] = _tier;
         }
         emit AddedToWhitelist(_userAddrs, _tier);
     }
